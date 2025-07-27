a961da67e06875ada772c12c1c35986b2013a281
jouaspiazu <joeaspiazu@hotmail.com>
2025-07-27 16:30:38 -0500

    Added swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Properties API')
+    .setDescription('Show CRUD of properties')
+    .setVersion('1.0')
+    .addTag('properties')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/properties/properties.controller.ts
+++ src/properties/properties.controller.ts
@@ -1,34 +1,83 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
 import { PropertiesService } from './properties.service';
 import { Property } from './entities/property.entity';
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 
+@ApiTags('properties')
 @Controller('properties')
 export class PropertiesController {
-    constructor(private readonly propertiesService: PropertiesService) {}
+  constructor(private readonly propertiesService: PropertiesService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all properties' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all properties',
+    type: [Property],
+  })
   findAll(): Promise<Property[]> {
     return this.propertiesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a property by ID' })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Property details', type: Property })
+  @ApiResponse({ status: 404, description: 'Property not found' })
   findOne(@Param('id') id: number): Promise<Property> {
     return this.propertiesService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new property' })
+  @ApiBody({ type: CreatePropertyDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The property has been created',
+    type: Property,
+  })
   create(@Body() createPropertyDto: CreatePropertyDto): Promise<Property> {
     return this.propertiesService.create(createPropertyDto);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() updatePropertyDto: UpdatePropertyDto): Promise<Property> {
+  @ApiOperation({ summary: 'Update a property' })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiBody({ type: UpdatePropertyDto })
+  @ApiResponse({
+    status: 200,
+    description: 'The property has been updated',
+    type: Property,
+  })
+  @ApiResponse({ status: 404, description: 'Property not found' })
+  update(
+    @Param('id') id: number,
+    @Body() updatePropertyDto: UpdatePropertyDto,
+  ): Promise<Property> {
     return this.propertiesService.update(id, updatePropertyDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a property' })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'The property has been deleted' })
+  @ApiResponse({ status: 404, description: 'Property not found' })
   remove(@Param('id') id: number): Promise<void> {
     return this.propertiesService.remove(id);
   }
